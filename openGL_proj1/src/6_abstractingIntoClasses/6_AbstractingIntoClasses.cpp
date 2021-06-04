1b2562cf40ac40aa8a24dce734c0b3a33fddc1c0
chat1891 <chatss1891@gmail.com>
2021-06-04 13:57:08 -0400

    render two objects

--- openGL_proj1/src/6_abstractingIntoClasses/6_AbstractingIntoClasses.cpp
+++ openGL_proj1/src/6_abstractingIntoClasses/6_AbstractingIntoClasses.cpp
@@ -56,10 +56,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-    100.0f, 100.0f,       0.0f, 0.0f,         1.f, 0.f, 0.f,1.f,// bottom left
-    200.0f, 100.0f,       1.0f, 0.0f,         0.f, 1.f, 0.f,1.f,// bottom right
-    200.0f, 200.0f,       1.0f, 1.0f,         0.f, 0.f, 1.f,1.f,// top right
-    100.0f, 200.0f,       0.0f, 1.0f,         1.f, 1.f, 0.f,1.f,// top left 
+    -50.0f, -50.0f,       0.0f, 0.0f,         1.f, 0.f, 0.f,1.f,// bottom left
+     50.0f, -50.0f,       1.0f, 0.0f,         0.f, 1.f, 0.f,1.f,// bottom right
+     50.0f,  50.0f,       1.0f, 1.0f,         0.f, 0.f, 1.f,1.f,// top right
+    -50.0f,  50.0f,       0.0f, 1.0f,         1.f, 1.f, 0.f,1.f,// top left 
     };
 
     unsigned int indices[] = {  // note that we start from 0!
@@ -108,7 +108,7 @@ int main(void)
 
     // view matrix
     //move camera to the right means move object to the left
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
     //5. Shader:
     Shader shader("res/shaders/Basic.Shader");
@@ -121,9 +121,6 @@ int main(void)
     Texture texture("res/textures/dog3.png");
     texture.Bind();
     shader.SetUniform1i("u_Texture", 0);
-
-
-
 
     //---------unbind all the buffer
     VA.Unbind();
@@ -140,7 +137,8 @@ int main(void)
     ImGui::StyleColorsDark();
     ImGui_ImplOpenGL3_Init((char*)glGetString(330));
 
-    glm::vec3 translation(200, 200, 0);
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 200, 0);
 
     float b = 0.0;
     float increment = 0.05f;
@@ -154,15 +152,23 @@ int main(void)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        glm::mat4 mvp = proj * view * model;
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+            glm::mat4 mvp = proj * view * model;
+            //----bind buffer for every frame
+            //shader.SetUniform4f("u_Color", 0.2, 0.3f, 0.0f, 1.0f);
+            shader.SetUniformMat4f("u_ModelViewProjectionMatrix", mvp);
+            renderer.Draw(VA, IB, shader);
+        }
 
-        //----bind buffer for every frame
-        //shader.SetUniform4f("u_Color", 0.2, 0.3f, b, 1.0f);
-        shader.SetUniform4f("u_Color", 0.4f, 0.541f, 0.659f, 1.0f);
-        shader.SetUniformMat4f("u_ModelViewProjectionMatrix", mvp);
-
-        renderer.Draw(VA, IB, shader);
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+            glm::mat4 mvp = proj * view * model;
+            //----bind buffer for every frame
+            //shader.SetUniform4f("u_Color", 0.2, 0.3f, b, 1.0f);
+            shader.SetUniformMat4f("u_ModelViewProjectionMatrix", mvp);
+            renderer.Draw(VA, IB, shader);
+        }
 
         //press 1 to draw line, press 2 to draw filled
         if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
@@ -177,7 +183,8 @@ int main(void)
         //show simple window of imgui
         {
             //pass in first memory address of translation. y and z will follow
-            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
         }
